900776a975a0975609c292d9b27de82004710400
nghia <tuannghia1406@gmail.com>
2022-07-08 16:35:11 +0700

    bon

--- resources/views/admin/login/signup.blade.php
+++ resources/views/admin/login/signup.blade.php
@@ -27,12 +27,12 @@
 
     <div class="container">
 
-        <div class="card o-hidden border-0 shadow-lg my-5">
+        <div class="card o-hidden border-0 shadow-lg my-5" style="width: 50%;margin-left:24%">
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
-                <div class="row">
-                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
-                    <div class="col-lg-7">
+                <div class="">
+                    {{-- <div class="col-lg-5 d-none d-lg-block bg-register-image"></div> --}}
+                    <div class="">
                         <div class="p-5">
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Đăng ký!</h1>
