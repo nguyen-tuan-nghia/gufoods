3aded136b358a99ebbc519ec712d0cda447a7ffc
nghia <tuannghia1406@gmail.com>
2022-07-08 16:04:57 +0700

    ba

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="card shadow mb-4">
     <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Quản lý sản phẩm</h6>
+        <h6 class="m-0 font-weight-bold text-primary">Quản lý đơn hàng</h6>
     </div>
     <div class="card-body">
         <div class="table-responsive">
--- resources/views/admin/post/category/index.blade.php
+++ resources/views/admin/post/category/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="card shadow mb-4">
     <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Quản lý sản phẩm</h6>
+        <h6 class="m-0 font-weight-bold text-primary">Quản lý danh mục bài viết</h6>
     </div>
     <div class="card-body">
         <div class="table-responsive">
